eec8d6a12fca5ee03261f2aa00b71229939325ce
GustavoGarciaPereira <gusgurtavo@gmail.com>
2020-06-22 21:25:01 -0300

    readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # desafio_Delivery_Much_Tech_Challenge
 desafio Delivery Much Tech Challenge
+
+
+## Essa é a minha resposta para o desafio
+
+
+
+bom começe clonadando o  meu repositorio
+
+git clone git@github.com:GustavoGarciaPereira/desafio_Delivery_Much_Tech_Challenge.git
+
+
+apos ter clonado com sucesso
+cove criar na raiz do projeto
+um arquivo chamado .env
+
+com o seguinte conteuro:
+
+API_GIF_KAY=sua_chave_giphy
+PORT=3000
+
+
+apos isso voce roda o comando docker build
+docker build -t gustavo/docker-node-tutorial . 
+
+apos rode o comento run do docker e veja o resultado
+
+docker run -p 3000:3000 gustavo/docker-node-tutorial
+
+http://localhost:3000/recipes/onion/tomato
--- src/index.js
+++ src/index.js
@@ -6,30 +6,27 @@ require('dotenv').config()
 const app = express();
 
 app.get('/recipes/:ingredient_1/:ingredient_2',async (request,response)=>{
-  //response.send(`<h1>Gustvo</h1>`)
   console.log(request.params.ingredient_1)
   console.log(request.params.ingredient_2)
-  const oi = await consulta_apis(response,{"ingredient_1":request.params.ingredient_1,
+  const resposta = await consulta_apis(response,{"ingredient_1":request.params.ingredient_1,
                           "ingredient_2":request.params.ingredient_2})
   
-  console.log(oi)
+  console.log(resposta)
 
 })
 
 
 async function consulta_apis(res,{ ingredient_1,ingredient_2 }){
     const comida = `http://www.recipepuppy.com/api/?i=${ingredient_1},${ingredient_2}&q=omelet&p=3`
-    //const gif = `http://api.giphy.com/v1/gifs/search?q=${formatar_title_gifs(data.results[0].title)}&api_key=goJ6l4rzB8TU9T4SD1VuxWCmLrvHeY9H&limit=1`
     let comidaOjs = new Object();
-    let comidaOjs2 = new Object();
+
 
     comidaOjs["keywords"]=[ingredient_1,ingredient_2]
     let list = []
-    let sei_la = await fetch(comida)
+    let resposta = await fetch(comida)
     .then(response => response.json())
     .then(data => {
       data = data.results
-      //console.log("old", data);
       return data;
     })
     .then(data => {
@@ -38,10 +35,6 @@ async function consulta_apis(res,{ ingredient_1,ingredient_2 }){
           .then(response => response.json())
           .then(data => {
 
-            //comidaOjs2["title"]=       e.title
-            //comidaOjs2["ingredients"]= formatar_ingredients(e.ingredients)
-            //comidaOjs2["link"]=        e.href,
-            //comidaOjs2["gif"]=         data.data[0].embed_url
             let returnedTarget = Object.assign({
              ["title"]: e.title,
              ["ingredients"]:formatar_ingredients(e.ingredients),
@@ -52,41 +45,26 @@ async function consulta_apis(res,{ ingredient_1,ingredient_2 }){
             list.push(returnedTarget)
 
             returnedTarget = {}
-
-            
-
-
              
             return list 
           }).then(x=>{
 
-            //res.json(returnedTarget)
             comidaOjs["recipes"] = x
             return comidaOjs
-            //console.log(x)
-            ////res.json(comidaOjs)
-            // 
-            //  //console.log(comidaOjs)
-            //  res.json(comidaOjs)
+
           }).then(x=>{
-
-            //res.json(returnedTarget)
-
             
             console.log(x)
             
-            ///res.json(x)
+
             res.status(200).json(x)
             return x
-            // 
-            //  //console.log(comidaOjs)
-            //  res.json(comidaOjs)
           })
 
         });
         return comidaOjs
       });
-      console.log("sd",sei_la)
+      console.log(resposta)
       
 }
 
@@ -116,5 +94,5 @@ function formatar_title_gifs(title){
 
 
 app.listen(process.env.PORT,()=>{
-    console.log("gusta")
+    console.log("gustavo")
 })
